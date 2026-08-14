a4f9849d87c79e6b796d765027905c3795523807
SDK Generator <sdk@nuki.io>
2026-08-14 11:44:58 +0000

    Regenerate from specs 20260814

--- client.go
+++ client.go
@@ -3,7 +3,7 @@ Nuki API
 
 The Nuki Web Api
 
-API version: 4.15.0
+API version: 4.17.0
 Contact: contact@nuki.io
 */
 
@@ -43,7 +43,7 @@ var (
 	queryDescape    = strings.NewReplacer( "%5B", "[", "%5D", "]" )
 )
 
-// APIClient manages communication with the Nuki API API v4.15.0
+// APIClient manages communication with the Nuki API API v4.17.0
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *Configuration
